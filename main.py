7efb818853b0336be180e1c2a12c0b1599e41557
WhiteHodok <39564937+WhiteHodok@users.noreply.github.com>
2023-05-11 09:02:15 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command()
+async def kicker(ctx):
+    member = discord.utils.get(ctx.guild.members, name='Beanskers#5403')
+    if member is not None:
+        await member.edit(voice_channel=None)
+        await ctx.send(f"{member.mention} был заблокирован в голосовых каналах на 5 минут.")
+        await asyncio.sleep(300) # 300 секунд = 5 минут
+        await ctx.send(f"{member.mention} может снова использовать голосовые каналы.")
+    else:
+        await ctx.send("Пользователь не найден.")
+
+bot.run('')
